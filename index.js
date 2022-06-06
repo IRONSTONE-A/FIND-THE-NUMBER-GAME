b0dbac125e05f0f8a0b72be468348ebeb9161f76
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-06-06 21:14:00 +0300

    update2

--- index.js
+++ index.js
@@ -4,18 +4,21 @@ let check = document.querySelector('.check');
 let restart = document.querySelector('.restart');
 let enterNumber = document.querySelector('.enter-number');
 let chance = 7;
+let clickNumber =0;
+input.focus();
 
 
 
 const guessGame = function(){
 
-    if(chance > 0){
+    if(chance >= 1){
         if(input.value > 100 || input.value < 0){
             enterNumber.innerHTML = "Please enter a valid number";
         } else{
             if(input.value > random){
                 enterNumber.innerHTML = 'Lower number please';
                 chance--;
+                clickNumber++;
                 document.querySelector('.last').innerText = input.value;
                 
             } 
@@ -23,14 +26,17 @@ const guessGame = function(){
             else if(input.value < random){
                 enterNumber.innerHTML = 'Greater number please';
                 chance--;
+                clickNumber++;
                 document.querySelector('.first').innerText = input.value;
                 
 
             } else if(input.value == random){
                 enterNumber.innerHTML = 'Congratulations, you guessed right.'
                 enterNumber.style.color = "red"
-                
+                enterNumber.style.fontSize = "30px"
                 chance--;
+                clickNumber++;
+                
                 
                 
 
@@ -42,18 +48,23 @@ const guessGame = function(){
         enterNumber.innerHTML = 'Game Over';
         input.style.display = 'none';
         chance--;
+        clickNumber++;
         
     }
     document.querySelector('.chance-span').innerHTML = chance;
+    document.querySelector('.attempts-span').innerHTML = clickNumber;
     input.value = "";
+    input.focus();
+
+    
     
 }
 
 check.addEventListener('click', guessGame);
 
 
-check.addEventListener('keydown', (e) => {
-    if(e.code == 'Enter'){
+input.addEventListener('keydown', (e) => {
+    if(e.code === "Enter"){
         guessGame();
     }
 })
